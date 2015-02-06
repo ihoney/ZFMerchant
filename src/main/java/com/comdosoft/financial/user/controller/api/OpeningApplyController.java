d0abf03ba96f88f6a1b12ae507ff832efd568641
鲜飞虎 <531950712@qq.com>
2015-02-06 13:37:11 +0800

    申请开通

---
+++ src/main/java/com/comdosoft/financial/user/controller/api/OpeningApplyController.java
@@ -0,0 +1,231 @@
+package com.comdosoft.financial.user.controller.api;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.comdosoft.financial.user.domain.Response;
+import com.comdosoft.financial.user.domain.zhangfu.Merchant;
+import com.comdosoft.financial.user.domain.zhangfu.OpeningApplie;
+import com.comdosoft.financial.user.service.OpeningApplyService;
+import com.comdosoft.financial.user.utils.Constants;
+import com.comdosoft.financial.user.utils.page.PageRequest;
+
+/**
+ * 
+ * 开通申请<br>
+ * <功能描述>
+ *
+ * @author xfh 2015年2月5日
+ *
+ */
+@RestController
+@RequestMapping(value = "apply")
+public class OpeningApplyController {
+
+	@Resource
+	private OpeningApplyService openingApplyService;
+
+	/**
+	 * 根据用户ID获得开通申请列表
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "applyList/{id}/{indexPage}", method = RequestMethod.GET)
+	public Response getApplyList(@PathVariable("id") Integer id,
+			@PathVariable("indexPage") Integer page) {
+
+		PageRequest PageRequest = new PageRequest(page, Constants.PAGE_SIZE);
+		Response response = new Response();
+
+		int offSetPage = PageRequest.getOffset();
+		response.setResult(openingApplyService.getApplyList(id, offSetPage,
+				Constants.PAGE_SIZE));
+		return response;
+
+	}
+
+	/**
+	 * 进入申请开通
+	 * 
+	 * @param id
+	 */
+	@RequestMapping(value = "applyDetails/{id}/{status}", method = RequestMethod.GET)
+	public Response getApplyDetails(@PathVariable("id") Integer id,
+			@PathVariable("status") Integer status) {
+		Response response = new Response();
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		// 获得终端详情
+		map.put("applyDetails", openingApplyService.getApplyDetails(id));
+		// 获得所有商户
+		map.put("merchants", openingApplyService.getMerchants());
+		// 数据回显(针对重新开通申请)
+		map.put("applyFor", openingApplyService.ReApplyFor(id));
+		// 材料名称
+		map.put("materialName", openingApplyService.getMaterialName(id, status));
+		response.setResult(map);
+		return response;
+	}
+
+	/**
+	 * 对公对私材料名称
+	 * 
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	@RequestMapping(value = "materialName/{id}/{status}", method = RequestMethod.GET)
+	public Response getMaterialName(@PathVariable("id") Integer id,
+			@PathVariable("status") Integer status) {
+		Response response = new Response();
+		response.setResult(openingApplyService.getMaterialName(id, status));
+		return response;
+	}
+
+	/**
+	 * 添加申请信息
+	 * 
+	 * @param paramMap
+	 */
+	@RequestMapping(value = "addApply", method = RequestMethod.POST)
+	@ResponseBody
+	public void addOpeningApply(@RequestBody List<Map<String, Object>> paramMap) {
+
+		OpeningApplie openingApplie = new OpeningApplie();
+		String status = null;
+		String openingAppliesId = null;
+		Integer terminalId = null;
+		String key = null;
+		String value = null;
+		int i = 0;
+		int y = 0;
+		for (Map<String, Object> map : paramMap) {
+			Set<String> keys = map.keySet();
+			if (y == 0) {
+				status = (String) map.get("status");
+				terminalId = (Integer) map.get("terminalId");
+				if ("重新申请".equals(status)) {
+					openingAppliesId = String.valueOf(openingApplyService
+							.getApplyesId(terminalId));
+					// 删除旧数据
+					openingApplyService.deleteOpeningInfos(Integer
+							.valueOf(openingAppliesId));
+				} else {
+					openingApplie
+							.setTerminalId((Integer) map.get("terminalId"));
+					openingApplie.setApplyCustomerId((Integer) map
+							.get("applyCustomerId"));
+					openingApplie.setPreliminaryVerifyUserId((Integer) map
+							.get("preliminaryVerifyUserId"));
+					openingApplie.setOpeningApplyMarkId((Integer) map
+							.get("openingApplyMarkId"));
+					openingApplyService.addOpeningApply(openingApplie);
+					openingAppliesId = String.valueOf(openingApplie.getId());
+				}
+			} else {
+				for (String str : keys) {
+					if (i == 0)
+						key = (String) map.get(str);
+					if (i == 1)
+						value = (String) map.get(str);
+					i++;
+				}
+				openingApplyService.addApply(key, value, openingAppliesId);
+				i = 0;
+			}
+			y++;
+		}
+	}
+
+	/**
+	 * 材料图片上传
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	@RequestMapping(value = "uploadFile", method = RequestMethod.POST)
+	@ResponseBody
+	public Response uploadFile(@RequestBody Map<String, String> map,
+			HttpServletRequest request) {
+		String filePath = null;
+		Set<String> keys = map.keySet();
+		for (String str : keys) {
+			filePath = map.get(str);
+		}
+		Response response = new Response();
+		String fileOutPath = request.getServletContext().getRealPath("/")
+				+ "WEB-INF/temp/";
+		int byteread = 0;// 读取的位数
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		File fileIn = new File(filePath);
+		File fileOut = new File(fileOutPath);
+
+		if (!fileOut.exists()) {
+			fileOut.mkdirs();
+		}
+		fileOut = new File(fileOutPath
+				+ filePath.substring(filePath.lastIndexOf("\\") + 1));
+		if (!fileIn.exists()) {
+			response.setMessage("该路径图片不存在！");
+			return response;
+		}
+		try {
+			in = new FileInputStream(fileIn);
+			out = new FileOutputStream(fileOut);
+			byte[] buffer = new byte[1024];
+			while ((byteread = in.read(buffer)) != -1) {
+				// 将读取的字节写入输出流
+				out.write(buffer, 0, byteread);
+			}
+			response.setMessage("上传成功！");
+			return response;
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setMessage("上传失败！");
+			return response;
+		} finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 根据商户id获得商户详细信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "Merchant", method = RequestMethod.GET)
+	public Response getMerchant(@PathVariable("id") Integer id) {
+		Response response = new Response();
+		Merchant merchant = new Merchant();
+		merchant = openingApplyService.getMerchant(id);
+		response.setResult(merchant);
+		return response;
+	}
+}
--- src/main/java/com/comdosoft/financial/user/interceptor/LoginInterceptor.java
+++ src/main/java/com/comdosoft/financial/user/interceptor/LoginInterceptor.java
@@ -1,23 +1,15 @@
 package com.comdosoft.financial.user.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import com.comdosoft.financial.user.service.SessionService;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(LoginInterceptor.class);
+	//private static final Logger LOG = LoggerFactory.getLogger(LoginInterceptor.class);
 	
-	private String loginUrl;
-	private SessionService sessionService;
+	//private String loginUrl;
+	//private SessionService sessionService;
 
-	@Override
+	/*@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 		boolean logined = sessionService.isLogined(request);
@@ -25,9 +17,9 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 			response.sendRedirect(request.getContextPath()+loginUrl);
 		}
 		return logined;
-	}
+	}*/
 
-	@Override
+	/*@Override
 	public void postHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
@@ -41,6 +33,6 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
 	public void setSessionService(SessionService sessionService) {
 		this.sessionService = sessionService;
-	}
+	}*/
 
 }
