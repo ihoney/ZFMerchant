09fbf9852e388c1f53c08c110ace8265f172ba7d
李增光 <465860728@qq.com>
2015-02-07 15:08:30 +0800

    交易流水测试问题修改

--- src/main/java/com/comdosoft/financial/user/service/trades/record/TradeRecordService.java
+++ src/main/java/com/comdosoft/financial/user/service/trades/record/TradeRecordService.java
@@ -1,5 +1,6 @@
 package com.comdosoft.financial.user.service.trades.record;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,9 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import com.comdosoft.financial.user.domain.Paging;
+import com.comdosoft.financial.user.domain.trades.TradeRecord;
 import com.comdosoft.financial.user.mapper.trades.record.TradeRecordMapper;
+import com.comdosoft.financial.user.mapper.zhangfu.TerminalsMapper;
 
 /**
  * 业务层实现类<br>
@@ -24,8 +27,11 @@ public class TradeRecordService {
     @Resource
     private TradeRecordMapper tradeRecordMapper;
 
+    @Resource
+    private TerminalsMapper terminalsMapper;
+
     public List<Map<Object, Object>> getTerminals(int customerId) {
-        return tradeRecordMapper.getTerminals(customerId);
+        return terminalsMapper.getTerminals(customerId);
     }
 
     public List<Map<Object, Object>> getTradeRecords(int tradeTypeId, String terminalNumber, String startTime, String endTime, int page, int rows) {
@@ -39,7 +45,27 @@ public class TradeRecordService {
         query.put("offset", paging.getOffset());
         query.put("rows", paging.getRows());
 
-        return tradeRecordMapper.getTradeRecords(query);
+        List<Map<Object, Object>> list = null;
+        switch (tradeTypeId) {
+        case TradeRecord.TRADETYPEID_1:
+            list = tradeRecordMapper.getTradeRecords12(query);
+        case TradeRecord.TRADETYPEID_2:
+            list = tradeRecordMapper.getTradeRecords12(query);
+            break;
+        case TradeRecord.TRADETYPEID_3:
+            list = tradeRecordMapper.getTradeRecords3(query);
+            break;
+        case TradeRecord.TRADETYPEID_4:
+            list = tradeRecordMapper.getTradeRecords45(query);
+            break;
+        case TradeRecord.TRADETYPEID_5:
+            list = tradeRecordMapper.getTradeRecords45(query);
+            break;
+        default:
+            list = new ArrayList<Map<Object, Object>>();
+            break;
+        }
+        return list;
     }
 
     public Map<Object, Object> getTradeRecordTotal(int tradeTypeId, String terminalNumber, String startTime, String endTime) {
