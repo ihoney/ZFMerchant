bdf87f6eb2ce2d5aeb03524f5fb9aae6d88872ca
李增光 <465860728@qq.com>
2015-03-03 20:49:24 +0800

    我的信息-地址管理

--- src/main/webapp/js/controllers/myinfoAddresses.js
+++ src/main/webapp/js/controllers/myinfoAddresses.js
@@ -3,7 +3,19 @@
 // 系统设置模块
 var myinfoAddressesModule = angular.module("myinfoAddressesModule", []);
 var myinfoAddressesController = function($scope, $http, LoginService) {
-	$scope.updatepassword = function() {
+	$scope.list = function() {
+		$http.get("api/customers/getAddressList/80").success(function(data) {
+			if (data.code == 1) {
+				$scope.list = data.result;
+			} else {
+				// 提示错误信息
+				alert(data.message);
+			}
+		}).error(function(data) {
+
+		});
+	};
+	$scope.update = function() {
 		$scope.updateCustomer = {
 			id : 8,
 			passwordOld : $scope.customer.passwordOld,
@@ -21,5 +33,18 @@ var myinfoAddressesController = function($scope, $http, LoginService) {
 
 		});
 	};
+	$scope.deleteAddress = function(e) {
+		$http.post("api/customers/updatePassword", $scope.updateCustomer).success(function(data) {
+			if (data.code == 1) { // 提示保存成功
+				alert("删除成功");
+			} else {
+				// 提示错误信息
+				alert(data.message);
+			}
+		}).error(function(data) {
+
+		});
+	};
+	$scope.list();
 };
 myinfoAddressesModule.controller("myinfoAddressesController", myinfoAddressesController);
