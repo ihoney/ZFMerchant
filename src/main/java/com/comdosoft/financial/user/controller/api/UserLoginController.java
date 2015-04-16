4fb138f593ec67e7e37c7a0210bf46e7d0cdcfba
harold <hudong@epalmpay.cn>
2015-04-16 15:44:37 +0800

    task 微调

--- src/main/java/com/comdosoft/financial/user/controller/api/UserLoginController.java
+++ src/main/java/com/comdosoft/financial/user/controller/api/UserLoginController.java
@@ -294,6 +294,7 @@ public class UserLoginController {
     public Response updatePassword(@RequestBody Customer customer) {
         try {
             if (userLoginService.findUname(customer.getUsername(),Customer.TYPE_CUSTOMER,Customer.STATUS_NORMAL) > 0) {
+            	System.out.println(userLoginService.findCode(customer)+"\t"+customer.getCode());
                 if (customer.getCode().equals(userLoginService.findCode(customer))) {
                     userLoginService.updatePassword(customer);
                     return Response.getSuccess("找回密码成功！");
