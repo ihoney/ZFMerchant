48099ddbffcf84d5a657e50d4e50d0b9e191c7a9
jwb445875 <445875775@qq.com>
2015-04-29 14:32:06 +0800

    11

--- src/main/java/com/comdosoft/financial/user/utils/RootUrl.java
+++ src/main/java/com/comdosoft/financial/user/utils/RootUrl.java
@@ -2,13 +2,17 @@ package com.comdosoft.financial.user.utils;
 
 public class RootUrl {
 
-//    public static String urlpath = "http://www.ebank007.com/";
-//
-//    public static String filepath = "http://file.ebank007.com/";
+    public static String urlpath = "http://www.ebank007.com/";
+
+    public static String filepath = "http://file.ebank007.com/";
 
     public static String localpath = "/opt/data/";
 
-    public static String urlpath = "http://121.40.84.2:8080/ZFMerchant/";
-
-    public static String filepath = "http://121.40.84.2:8888/";
+//    public static String urlpath = "http://121.40.84.2:8080/ZFMerchant/";
+//
+//    public static String filepath = "http://121.40.84.2:8888/";
+    
+//    public static String urlpath = "http://114.251.149.242:18080/ZFMerchant/";
+//
+//    public static String filepath = "http://121.40.84.2:8888/";
 }
