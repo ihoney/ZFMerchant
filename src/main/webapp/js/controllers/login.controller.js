b12c30fdbc9e384897bb8960d3e556afaf010f48
鲜飞虎 <531950712@qq.com>
2015-05-12 13:14:42 +0800

    回车事件

--- src/main/webapp/js/controllers/login.controller.js
+++ src/main/webapp/js/controllers/login.controller.js
@@ -237,6 +237,15 @@ var loginController = function($scope, $location, $http, LoginService) {
 			LoginService.login($scope, $http);
 		}
 	};
+	
+	 //登陆回车事件 
+	 document.onkeydown=function(event){
+        var e = event || window.event || arguments.callee.caller.arguments[0];
+         if(e && e.keyCode==13){ // enter 键
+             //要做的事情
+       	  $('#dengluenter').click();
+        }
+    }; 
 
 	var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
 	$scope.loginUserName = LoginService.loginUserName;
