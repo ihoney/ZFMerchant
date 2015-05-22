f396c0643db77f7f70eb09af1af3ce0ad1de72bd
jwb445875 <445875775@qq.com>
2015-05-22 13:33:13 +0800

    left

--- src/main/webapp/js/controllers/head.controller.js
+++ src/main/webapp/js/controllers/head.controller.js
@@ -18,6 +18,29 @@ var lowstocksController = function ($scope) {
 var userdownController = function ($scope) {
 	$scope.$emit('topTitle',"华尔街金融平台-app下载");
 };
+var leftController = function ($scope) {
+	//左侧样式调整
+	$("#left_common li").unbind("click").bind("click", function(){
+		$(this).children('a').addClass("hover");
+		$(this).siblings().children('a').removeClass("hover");
+		if (!$(this).hasClass("second") ){ //判断是否有子节点
+			if ( !$(this).parents().hasClass("second") ){
+				$(".second").children('ol').children('li').children('a').removeClass("hover");
+			}
+		}
+   });
+	
+	/*------用户后台导航菜单--------*/
+	$("li.second > a:nth-child(1)").click(function(){
+		$(this).parent().find("ol").toggle();
+		if(!$(this).parent().find("ol").is(":visible")){
+			$(this).find("i").removeClass("on").addClass("off");
+		}else{
+			$(this).find("i").removeClass("off").addClass("on");
+		}
+	});
+};
+
 headController.$inject = ['$scope','LoginService'];
 headModule.controller("headController", headController);
 
--- src/main/webapp/js/controllers/myapp.controller.js
+++ src/main/webapp/js/controllers/myapp.controller.js
@@ -5,26 +5,6 @@ var myappModule = angular.module("myappModule",[]);
 
 var myappController = function ($scope, $http, LoginService) {
 	$scope.$emit('topTitle',"华尔街金融平台-我的掌富");
-		//左侧样式调整
-		$("#left_common li").unbind("click").bind("click", function(){
-			$(this).children('a').addClass("hover");
-			$(this).siblings().children('a').removeClass("hover");
-			if (!$(this).hasClass("second") ){ //判断是否有子节点
-				if ( !$(this).parents().hasClass("second") ){
-					$(".second").children('ol').children('li').children('a').removeClass("hover");
-				}
-			}
-	   });
-		
-		/*------用户后台导航菜单--------*/
-		$("li.second > a:nth-child(1)").click(function(){
-			$(this).parent().find("ol").toggle();
-			if(!$(this).parent().find("ol").is(":visible")){
-				$(this).find("i").removeClass("on").addClass("off");
-			}else{
-				$(this).find("i").removeClass("off").addClass("on");
-			}
-		});
 	$scope.my_message_list = function(){
 		$scope.req={customer_id:LoginService.userid,rows:8};
 		$http.post("api/message/receiver/getAll", $scope.req).success(function (data) {   
